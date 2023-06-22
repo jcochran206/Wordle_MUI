e97fa4a365fada5872bc5f1ee3f53f8d8cbfdf88
jcochran206 <jonathan.o.cochran@gmail.com>
2023-06-21 23:31:32 -0700

    deployed to vercel

--- src/components/WordleBoard/WordleBoard.tsx
+++ src/components/WordleBoard/WordleBoard.tsx
@@ -2,11 +2,32 @@ import React, { useState, useEffect } from 'react';
 import WordleKeyBoard from '../WordleKeyBoard/WordleKeyBoard';
 import WordleInput from '../WordleInput/WordleInput';
 
+const API_URL = 'https://wordle-answers-solutions.p.rapidapi.com/today'
+
+// const options = {
+//     method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': process.env.WORDLE_API_KEY,
+// 		'X-RapidAPI-Host': process.env.WORDLE_HOST
+// }
+
+// interface Props {
+//     options: string[];
+   
+// }
 
 const WordleBoard: React.FC = (): JSX.Element => {
     const [guesses, setGuesses] = useState<string[]>([...Array(6)]);
     const [solution, setSolution] = useState<string>("");
     //fetch data from api
+    // useEffect(() => {
+    //     const fetchWords = async () => {
+    //         const response = await fetch(API_URL, options);
+    //         const words = await response.json();
+    //         setSolution(words[Math.floor(Math.random() * words.length)].toLowerCase())
+    //     };
+    //     fetchWords()
+    // }, [])
     
   return (
     <div>
