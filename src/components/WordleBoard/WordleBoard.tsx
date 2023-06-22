d61ae4b569a42805519e88c678a4791976c1bd54
jcochran206 <jonathan.o.cochran@gmail.com>
2023-06-21 23:07:44 -0700

    added .env file for environment variables

--- src/components/WordleBoard/WordleBoard.tsx
+++ src/components/WordleBoard/WordleBoard.tsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import WordleKeyBoard from '../WordleKeyBoard/WordleKeyBoard';
 import WordleInput from '../WordleInput/WordleInput';
 
 
 const WordleBoard: React.FC = (): JSX.Element => {
-    const [guesses, setGuesses] = useState<string[]>([...Array(6)])
+    const [guesses, setGuesses] = useState<string[]>([...Array(6)]);
+    const [solution, setSolution] = useState<string>("");
+    //fetch data from api
+    
   return (
     <div>
         {guesses.map((guess: string, i: number): JSX.Element => (
-            <WordleInput index={i} key={i}/>
+            <WordleInput index={i} key={i} setGuesses={setGuesses} guesses={guesses}/>
         ))}
         <WordleKeyBoard />
     </div>
--- src/components/WordleInput/WordleInput.tsx
+++ src/components/WordleInput/WordleInput.tsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 
 interface Props {
     index: number;
+    setGuesses: (guesses: string[]) => void;
+    guesses: string[];
 }
 
 const WordleInput: React.FC<Props> = ({index}: Props): JSX.Element => {
@@ -11,16 +13,24 @@ const WordleInput: React.FC<Props> = ({index}: Props): JSX.Element => {
     const autoTab = (inputIndex: number, guessIndex: number):void => {
         document.getElementById(`${inputIndex}${guessIndex}`)?.focus()
     }
+    //handle keyup from keyboard function 
     const handleKeyUp = (e: React.KeyboardEvent, i: number): void => {
+        let isCurrentGuessFull: boolean = currentGuess.filter((letter: string): string => letter && letter).length === 5 ? true : false;
         if(e.key === "BackSpace"){
             let inputToGoToIndex: number = i - 1 >=0 ? i-1 : i
             autoTab(inputToGoToIndex, index)
-        }else if(i === 4 && e.key === "Enter"){
+        }else if(i === 4 && e.key === "Enter" && isCurrentGuessFull){
+            handleSubmit();
             autoTab(0, index +1)
         }else {
             let inputToGoToIndex: number = i + 1
             autoTab(inputToGoToIndex, index)
         }
+    }
+
+    //handle submission 
+    const handleSubmit = (): void => {
+
     }
   return (
     <div>
